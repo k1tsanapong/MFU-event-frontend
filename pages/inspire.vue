05be869ee33456c5e0cd9a797683aae9db821a57
Tae <6431503042@lamduan.mfu.ac.th>
2023-11-12 12:37:14 +0700

    yo12

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,25 +1,58 @@
+import { createVuetify} from 'vuetify'
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <v-app>
+    <!-- Your custom app bar goes here -->
+    <v-app-bar app color="#8F2B31"></v-app-bar>
+    <v-container class="search-container">
+      <!-- Search bar and button in the same column -->
+      <v-col xs="12" sm="8">
+        <v-row align="center">
+          <v-col>
+            <v-text-field
+              v-model="search"
+              label="Search"
+              class="search-text-field"
+              solo-inverted
+              hide-details
+              rounded
+              outlined
+            >
+              <template v-slot:append>
+                <v-icon>mdi-magnify</v-icon>
+              </template>
+            </v-text-field>
+          </v-col>
+          <v-col class="text-right" xs="12" sm="4">
+            <v-btn @click="handleSort"
+              >Sort
+              <template>
+                <v-icon>mdi-sort</v-icon>
+              </template>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'InspirePage'
-}
+  data() {
+    return {
+      search: "",
+    };
+  },
+};
 </script>
+
+<style>
+.search-container {
+  background-color: #bc8e5d;
+  border-radius: 50px;
+  width: 80%;
+}
+.search-text-field {
+  background-color: white;
+}
+</style>
