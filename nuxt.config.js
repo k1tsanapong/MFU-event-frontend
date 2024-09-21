fbffa3a00505ddc1770db28bac67696134ae8dc7
Seki Darubi <6431503002@lamduan.mfu.ac.th>
2024-09-21 13:08:51 +0700

    cloudbuild

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,7 +71,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.AXIOS_URL || "/",
+    baseURL: process.env.BASE_URL || "/",
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
